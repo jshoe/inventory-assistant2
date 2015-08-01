4eb0813cd69c08755cb7c1818825481e2282f18e
portdevil <portdevil@gmail.com>
2015-07-31 18:04:51 -0700

    Change MainActivity layout to make_new_grp

--- mobile/src/main/java/com/example/jonathan/inventoryassistant/MainActivity.java
+++ mobile/src/main/java/com/example/jonathan/inventoryassistant/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jonathan.inventoryassistant;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_make_new_grp);
     }
 
     @Override
