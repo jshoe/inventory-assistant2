ad0bbef6246a386f93105dbdaea3d73e3a4ae4f8
portdevil <portdevil@gmail.com>
2015-07-31 17:41:14 -0700

    Fix null NfcAdapter problem

--- mobile/src/main/java/com/example/jonathan/inventoryassistant/TestActivity.java
+++ mobile/src/main/java/com/example/jonathan/inventoryassistant/TestActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class TestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
     }
 
     @Override
